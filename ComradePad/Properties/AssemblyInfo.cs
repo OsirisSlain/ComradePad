47c8f6a3211ba04a541f985db8b65cc2cac000f3
Blake Shaw <blake@esonus.com>
2020-10-23 16:48:20 -0600

    Initial checkin of empty WinForms application

---
+++ ComradePad/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ComradePad")]
+[assembly: AssemblyDescription("ComradePad encodes and decodes files using one-time pads")]
+[assembly: AssemblyCompany("Blake Shaw")]
+[assembly: AssemblyProduct("ComradePad")]
+[assembly: AssemblyCopyright("Copyright © 2020 Blake Shaw")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3833b305-1e83-4f09-a0e0-d69156da4061")]
+
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+[assembly: AssemblyVersion("0.1.*")]
